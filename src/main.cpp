bdfe50c3150230c7cb0811997884649aa7bfac6d
Diego Reis <reisdiego144@gmail.com>
2025-05-13 19:19:10 -0300

    Formatting

--- src/main.cpp
+++ src/main.cpp
@@ -61,12 +61,13 @@ float gravitational_force = 0.5f;
 
 const std::string LOREM_IPSUM = "Lorem ipsum dolor sit amet consectetur adipiscing elit. Blandit quis suspendisse aliquet nisi sodales consequat magna. Sem placerat in id cursus mi pretium tellus. Finibus facilisis dapibus etiam interdum tortor ligula congue. Sed diam urna tempor pulvinar vivamus fringilla lacus. Porta elementum a enim euismod quam justo lectus. Nisl malesuada lacinia integer nunc posuere ut hendrerit. Imperdiet mollis nullam volutpat porttitor ullamcorper rutrum gravida. Ad litora torquent per conubia nostra inceptos himenaeos. Ornare sagittis vehicula praesent dui felis venenatis ultrices. Dis parturient montes nascetur ridiculus mus donec rhoncus. Potenti ultricies habitant morbi senectus netus suscipit auctor. Maximus eget fermentum odio phasellus non purus est. Platea dictumst lorem ipsum dolor sit amet consectetur. Dictum risus blandit quis suspendisse aliquet nisi sodales. Vitae pellentesque sem placerat in id cursus mi. Luctus nibh finibus facilisis dapibus etiam interdum tortor. Eu aenean sed diam urna tempor pulvinar vivamus. Tincidunt nam porta elementum a enim euismod quam. Iaculis massa nisl malesuada lacinia integer nunc posuere. Velit aliquam imperdiet mollis nullam volutpat porttitor ullamcorper. Taciti sociosqu ad litora torquent per conubia nostra. Primis vulputate ornare sagittis vehicula praesent dui felis. Et magnis dis parturient montes nascetur ridiculus mus. Accumsan maecenas potenti ultricies habitant morbi senectus netus. Mattis scelerisque maximus eget fermentum odio phasellus non. Hac habitasse platea dictumst lorem ipsum dolor sit. Vestibulum fusce dictum risus blandit quis suspendisse aliquet. Ex sapien vitae pellentesque sem placerat in id. Neque at luctus nibh finibus facilisis dapibus etiam. Tempus leo eu aenean sed diam urna tempor. Viverra ac tincidunt nam porta elementum a enim. Bibendum egestas iaculis massa nisl malesuada lacinia integer. Arcu dignissim velit aliquam imperdiet mollis nullam volutpat. Class aptent taciti sociosqu ad litora torquent per. Turpis fames primis vulputate ornare sagittis vehicula praesent. Natoque penatibus et magnis dis parturient montes nascetur. Feugiat tristique accumsan maecenas potenti ultricies habitant morbi. Nulla molestie mattis scelerisque maximus eget fermentum odio. Cubilia curae hac habitasse platea dictumst lorem ipsum. Mauris pharetra vestibulum fusce dictum risus blandit quis. Quisque faucibus ex sapien vitae pellentesque sem placerat. Ante condimentum neque at luctus nibh finibus facilisis. Duis convallis tempus leo eu aenean sed diam. Sollicitudin erat viverra ac tincidunt nam porta elementum. Nec metus bibendum egestas iaculis massa nisl malesuada. Commodo augue arcu dignissim velit aliquam imperdiet mollis. Semper vel class aptent taciti sociosqu ad litora. Cras eleifend turpis fames primis vulputate ornare sagittis. Orci varius natoque penatibus et magnis dis parturient. Proin libero feugiat tristique accumsan maecenas potenti ultricies. Eros lobortis nulla molestie mattis scelerisque maximus eget. Curabitur facilisi cubilia curae hac habitasse platea dictumst. Efficitur laoreet mauris pharetra vestibulum fusce dictum risus. Adipiscing elit quisque faucibus ex sapien vitae pellentesque. Consequat magna ante condimentum neque at luctus nibh. Pretium tellus duis convallis tempus leo eu aenean. Ligula congue sollicitudin erat viverra ac tincidunt nam. Fringilla lacus nec metus bibendum egestas iaculis massa. Justo lectus commodo augue arcu dignissim velit aliquam. Ut hendrerit semper vel class aptent taciti sociosqu. Rutrum gravida cras eleifend turpis fames primis vulputate. Inceptos himenaeos orci varius natoque penatibus et magnis. Venenatis ultrices proin libero feugiat tristique accumsan maecenas. Donec rhoncus eros lobortis nulla molestie mattis scelerisque. Suscipit auctor curabitur facilisi cubilia curae hac habitasse. Purus est efficitur laoreet mauris pharetra vestibulum fusce. Amet consectetur adipiscing elit quisque faucibus ex sapien. Nisi sodales consequat magna ante condimentum neque at. Cursus mi pretium tellus duis convallis tempus leo. Interdum tortor ligula congue sollicitudin erat viverra ac. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Euismod quam justo lectus commodo augue arcu dignissim. Nunc posuere ut hendrerit semper vel class aptent. Porttitor ullamcorper rutrum gravida cras eleifend turpis fames. Conubia nostra inceptos himenaeos orci varius natoque penatibus. Dui felis venenatis ultrices proin libero feugiat tristique. Ridiculus mus donec rhoncus eros lobortis nulla molestie. Senectus netus suscipit auctor curabitur facilisi cubilia curae. Phasellus non purus est efficitur laoreet mauris pharetra. Dolor sit amet consectetur adipiscing elit quisque faucibus. Suspendisse aliquet nisi sodales consequat magna ante condimentum. In id cursus mi pretium tellus duis convallis. Dapibus etiam interdum tortor ligula congue sollicitudin erat. Urna tempor pulvinar vivamus fringilla lacus nec metus. Aenim euismod quam justo lectus commodo augue. Lacinia integer nunc posuere ut hendrerit semper vel. Nullam volutpat porttitor ullamcorper rutrum gravida cras eleifend. Torquent per conubia nostra inceptos himenaeos orci varius. Vehicula praesent dui felis venenatis ultrices proin libero. Montes nascetur ridiculus mus donec rhoncus eros lobortis. Habitant morbi senectus netus suscipit auctor curabitur facilisi. Fermentum odio phasellus non purus est efficitur laoreet. Lorem ipsum dolor sit amet consectetur adipiscing elit. Blandit quis suspendisse aliquet nisi sodales consequat magna. Sem placerat in id cursus mi pretium tellus. Finibus facilisis dapibus etiam interdum tortor ligula congue. Sed diam urna tempor pulvinar vivamus fringilla lacus. Porta elementum a enim euismod quam justo lectus. Nisl malesuada lacinia integer nunc posuere ut hendrerit.";
 const int LAST_STEP = 100; // only for testing, probably this won't be known at compile-time
-struct pagerank_step_context {
+struct pagerank_step_context
+{
     int current_step = 0;
     int total_steps = LAST_STEP;
-    
+
     // could be a function that lazy-loads the description instead
-    std::vector<std::string> step_description; 
+    std::vector<std::string> step_description;
 } step;
 
 Universe universe(graph,
@@ -215,7 +216,8 @@ void mouse_button_callback(GLFWwindow *window, int button, int action, int mods)
 {
 }
 
-struct imgui_context {
+struct imgui_context
+{
     bool showing_about = false;
     bool last_frame_showing_about = false;
     ImVec2 steps_window_start_pos;
@@ -228,27 +230,27 @@ void framebuffer_size_callback(GLFWwindow *window, int width, int height)
     camera.set_viewport(width, height);
 }
 
-
-
-void imgui_update_frame() 
+void imgui_update_frame()
 {
 
-    
-    if(ImGui::BeginMainMenuBar()) {
-        if(ImGui::BeginMenu("Options")) {
-            if(ImGui::MenuItem("Regenerate graph"))
+    if (ImGui::BeginMainMenuBar())
+    {
+        if (ImGui::BeginMenu("Options"))
+        {
+            if (ImGui::MenuItem("Regenerate graph"))
                 init_graph();
 
-            if(ImGui::MenuItem("Toggle rotation"))
+            if (ImGui::MenuItem("Toggle rotation"))
                 autoRotateX = !autoRotateX;
-                
-            //if(ImGui::MenuItem("Keyboard settings")) {}
 
-            imgui_context.showing_steps_window  |= ImGui::MenuItem("Show steps window");
+            // if(ImGui::MenuItem("Keyboard settings")) {}
+
+            imgui_context.showing_steps_window |= ImGui::MenuItem("Show steps window");
             ImGui::EndMenu();
         }
 
-        if(ImGui::BeginMenu("Help")) {
+        if (ImGui::BeginMenu("Help"))
+        {
             imgui_context.showing_about |= ImGui::MenuItem("About");
             ImGui::EndMenu();
         }
@@ -256,49 +258,51 @@ void imgui_update_frame()
         ImGui::EndMainMenuBar();
     }
 
-    if(imgui_context.showing_about && !imgui_context.last_frame_showing_about)
+    if (imgui_context.showing_about && !imgui_context.last_frame_showing_about)
         ImGui::OpenPopup("About");
-    if(ImGui::BeginPopupModal("About", &imgui_context.showing_about, ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoMove)) {
+    if (ImGui::BeginPopupModal("About", &imgui_context.showing_about, ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoMove))
+    {
         ImGui::Text("This application visualizes the PageRank algorithm step-by-step using OpenGL to render an interactive graph.");
         ImGui::Text("Want to contribute or explore the code?");
         ImGui::Text("Check out the project on GitHub:");
         ImGui::TextLinkOpenURL("https://github.com/el-yawd/page-rank", "https://github.com/el-yawd/page-rank");
-        if(ImGui::Button("Close")) {
+        if (ImGui::Button("Close"))
+        {
             imgui_context.showing_about = false;
         }
         ImGui::EndPopup();
     }
     imgui_context.last_frame_showing_about = imgui_context.showing_about;
 
-    
     ImGui::SetNextWindowSizeConstraints(ImVec2(300, 125), ImVec2(300, 250));
-    if(imgui_context.showing_steps_window && ImGui::Begin("PageRank Step Descriptor", &imgui_context.showing_steps_window, ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoCollapse)) {
+    if (imgui_context.showing_steps_window && ImGui::Begin("PageRank Step Descriptor", &imgui_context.showing_steps_window, ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoCollapse))
+    {
         ImGui::SetWindowPos(imgui_context.steps_window_start_pos, ImGuiCond_FirstUseEver);
         ImGui::Text(std::format("Step: {}/{}", step.current_step, step.total_steps).c_str());
         ImGui::TextWrapped(step.step_description[step.current_step].c_str());
         ImGui::Dummy(ImVec2(0, 10));
-    
-        if(ImGui::Button("Prev") && step.current_step > 0)
+
+        if (ImGui::Button("Prev") && step.current_step > 0)
             step.current_step--;
         ImGui::SameLine();
-        if(ImGui::Button("Next") && step.current_step < step.total_steps)
+        if (ImGui::Button("Next") && step.current_step < step.total_steps)
             step.current_step++;
-        
+
         ImGui::End();
     }
-    
 }
 
-std::string get_current_executable_path() {
-    #if defined(unix) || defined(__unix__) || defined(__unix)
-        return std::filesystem::canonical(std::filesystem::path("/proc/self/exe")).parent_path();
-    #endif
-    #if defined(_WIN32) || defined(WIN32)
-        // WARNING: Not tested on Windows
-        char path[MAX_PATH];
-        GetModuleFileNameA(NULL, path, MAX_PATH);
-        return std::filesystem::path(path).parent_path().string();
-    #endif
+std::string get_current_executable_path()
+{
+#if defined(unix) || defined(__unix__) || defined(__unix)
+    return std::filesystem::canonical(std::filesystem::path("/proc/self/exe")).parent_path();
+#endif
+#if defined(_WIN32) || defined(WIN32)
+    // WARNING: Not tested on Windows
+    char path[MAX_PATH];
+    GetModuleFileNameA(NULL, path, MAX_PATH);
+    return std::filesystem::path(path).parent_path().string();
+#endif
 
     return "";
 }
@@ -308,11 +312,11 @@ int main()
     srand(time(NULL));
     // Only for testing, we'll have to do it another way!
     step.step_description.resize(step.total_steps);
-    for(int i = 0; i < step.total_steps; i++)
-        step.step_description[i] = LOREM_IPSUM.substr(rand() % (LOREM_IPSUM.size()-60), 60);
+    for (int i = 0; i < step.total_steps; i++)
+        step.step_description[i] = LOREM_IPSUM.substr(rand() % (LOREM_IPSUM.size() - 60), 60);
 
     cout << "### Starting Page Rank ###" << endl;
-    
+
     cout << "Initializing graph..." << endl;
     init_graph();
 
@@ -325,7 +329,7 @@ int main()
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
     glfwWindowHint(GLFW_CLIENT_API, GLFW_OPENGL_ES_API);
-    imgui_context.steps_window_start_pos = ImVec2(10, WIN_HEIGHT-100);
+    imgui_context.steps_window_start_pos = ImVec2(10, WIN_HEIGHT - 100);
     window = glfwCreateWindow(WIN_WIDTH, WIN_HEIGHT, "Graph Viewer", NULL, NULL);
 
     if (!window)
@@ -350,28 +354,28 @@ int main()
     // Init ImGui
     IMGUI_CHECKVERSION();
     ImGui::CreateContext();
-    ImGuiIO& io = ImGui::GetIO(); (void)io;
+    ImGuiIO &io = ImGui::GetIO();
+    (void)io;
     io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;
-    
-    
+
     ImGui_ImplGlfw_InitForOpenGL(window, true); // Send GLFW context to ImGui
     ImGui_ImplOpenGL3_Init("#version 300 es");  // Set ImGui OpenGL version
 
     // Change ImGui default theme
     ImGui::StyleColorsDark();
-    
+
     // Disable ImGui config saving
     ImGui::GetIO().IniFilename = NULL;
     ImGui::GetIO().LogFilename = NULL;
 
     // Import and use the font
     std::string current_path = get_current_executable_path();
-    if(current_path != "") {
-        ImFont* font = io.Fonts->AddFontFromFileTTF((current_path + "/fonts/Roboto-Regular.ttf").c_str(), 15.0f, NULL, io.Fonts->GetGlyphRangesDefault());
-        if(font != nullptr)
+    if (current_path != "")
+    {
+        ImFont *font = io.Fonts->AddFontFromFileTTF((current_path + "/fonts/Roboto-Regular.ttf").c_str(), 15.0f, NULL, io.Fonts->GetGlyphRangesDefault());
+        if (font != nullptr)
             io.FontDefault = font;
     }
-
 
     projection = glm::perspective(
         glm::radians<float>(60.0f), (GLfloat)WIN_WIDTH / (GLfloat)WIN_HEIGHT,
@@ -390,7 +394,7 @@ int main()
     double currentTime = glfwGetTime();
     double startTime = glfwGetTime();
     double initTime = glfwGetTime();
-    
+
     while (!glfwWindowShouldClose(window))
     {
         timeSimulatedThisIteration = 0;
@@ -413,7 +417,7 @@ int main()
         ImGui_ImplOpenGL3_NewFrame();
         ImGui_ImplGlfw_NewFrame();
         ImGui::NewFrame();
-        
+
         imgui_update_frame();
 
         ImGui::Render();
